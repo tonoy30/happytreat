61762b15bc7b9e915bd35f35083ea3d08100947b
Ali Ahmmed Tonoy <tonoy.sust@gmail.com>
2019-04-19 04:13:26 +0600

    auth setting

--- WasteFood/settings.py
+++ WasteFood/settings.py
@@ -123,3 +123,12 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, static),
 # ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+}
