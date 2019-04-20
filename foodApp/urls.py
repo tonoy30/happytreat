6534f2c72d23457454ed5213c7d8e5750f77aa6f
Ali Ahmmed Tonoy <tonoy.sust@gmail.com>
2019-04-20 08:15:30 +0600

    website

--- foodApp/urls.py
+++ foodApp/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login', loginView, {
+         'template_name': 'login.html'}, name='login'),
+    path('leaderboard/', views.leaderboard, name='leaderboard'),
+    path('signup/', views.signup, name='signup'),
+    path('forget_password/', views.forget_password, name='forget_password'),
+    path('about_us/', views.about_us, name='about_us')
 ]
--- foodApp/views.py
+++ foodApp/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import FoodRequest, Profile
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html', {})
+
+
+def login_view(request):
+    return render(request, 'login.html', {})
+
+
+def signup(request):
+    return render(request, 'register.html', {})
+
+
+def forget_password(request):
+    return render(request, 'forgot-password.html', {})
+
+
+def about_us(request):
+    return render(request, 'about_us.html', {})
+
+
+def leaderboard(request):
+    donator_list = []
+    donator_name = []
+    data = FoodRequest.objects.order_by('quantity').values(
+        'quantity').values('donator__user')
+    for m in data:
+        for k, v in m.items():
+            print(k, v)
+            donator_list.append(v)
+    for i in range(len(donator_list)):
+        donator = Profile.objects.filter(user_id=donator_list[i]).values()
+        for m in donator:
+            for k, v in m.items():
+                print(k, v)
+                donator_name.append(v)
+    return render(request, 'leaderboard.html', {'donator_list': donator_list})
